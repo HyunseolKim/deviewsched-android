fc32116ff6e77b45ac1c689392989adf333869f0
Hyunseol Kim <hyunseol518@naver.com>
2015-08-24 19:49:03 +0900

    AuthorizationHelper 추가

---
+++ app/src/main/java/com/gdgssu/android_deviewsched/helper/AuthorizationHelper.java
@@ -0,0 +1,11 @@
+package com.gdgssu.android_deviewsched.helper;
+
+/**
+ * Created by flashgugu on 15. 8. 24.
+ */
+public class AuthorizationHelper {
+    private String url;
+    private String method;
+    private String signature_data;
+    private String signature;
+}
